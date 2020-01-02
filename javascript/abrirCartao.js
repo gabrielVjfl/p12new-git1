633dcf03c9d090d94f2fa370e81f5d69c972685c
gabrielVjfl <57651924+gabrielVjfl@users.noreply.github.com>
2020-01-02 16:52:11 -0300

    subindo arquivos no git

---
+++ javascript/abrirCartao.js
@@ -0,0 +1,12 @@
+//function abrirCartao() {
+
+   // let endereco = document.getElementById("cartao")
+
+   // if(endereco.style.top == "50px") {
+   //    endereco.style.top = '-700px'
+   // }
+  //  else {
+        endereco.style.top = "50px"
+    //}
+//}
+
